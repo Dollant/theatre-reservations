f073f1125795dc4d4edf3bfcacf6a24a2117ad33
Dollant <ariawilliams2425@gmail.com>
2026-04-14 11:27:19 -0400

    Add name input validation ensuring the user enters at least a first and last name.

--- src/main/java/com/pluralsight/TheatreReservationsApp.java
+++ src/main/java/com/pluralsight/TheatreReservationsApp.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class TheatreReservationsApp {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        String fullName;
+        while (true) {
+            System.out.print("Please enter your name ✩: ");
+            fullName = keyboard.nextLine().trim();
+
+            if (fullName.contains(" ") && fullName.split(" ").length >= 2) {
+                break;
+            } else {
+                System.out.println("Please enter first and last name .✦ ݁˖");
+            }
+        }
+
+
+
+    }
 }
